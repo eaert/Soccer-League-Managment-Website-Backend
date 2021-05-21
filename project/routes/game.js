a8182d4046e3b41046ba3d703cd3c00b87fc87f1
eaert <62422332+eaert@users.noreply.github.com>
2021-05-21 15:28:45 +0300

    updateScore and setReferee functions. as well the support functions in game_utils.

--- project/routes/game.js
+++ project/routes/game.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var router = express.Router();
 const game_utils = require("./utils/game_utils");
-var generateGameID = 0;
 
 router.get("/addGame", async (req, res, next) => {
   try {
@@ -11,11 +10,29 @@ router.get("/addGame", async (req, res, next) => {
     if (req.body.homeTeamGoals < 0 || req.body.awayTeamGoals < 0){
         throw { status: 400, message: "Team's Goals must be positive number"}
     }
-    await DButils.execQuery(
-        `insert into Game values ('${generateGameID++}',${req.body.time}, ${req.body.homeTeam}, ${req.body.awayTeam},
-         ${req.body.homeTeamGoals}, ${req.body.awayTeamGoals}, ${req.body.field}, ${req.body.referee})`
-      );
-      res.status(201).send("Game created");
+    await game_utils.createGame(req.body);
+    res.status(201).send("Game created");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/updateScore", async (req, res, next) => {
+  try {
+    await game_utils.updateGameScore(req.body);
+    res.status(201).send("Game Score been update.");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/setReferee", async (req, res, next) => {
+  try {
+    if (game_utils.isRefereeExist(req.data.referee)){
+      throw { status: 400, message: "Requested Referee doens't exist"}
+    }
+    await game_utils.setReferee(req.body);
+    res.status(201).send("Game's referee been update.");
   } catch (error) {
     next(error);
   }
